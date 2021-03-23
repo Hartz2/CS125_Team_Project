0518c86868e7072314a77e2a47939727b9a7e54c
A-Morgan47 <morgaa17@prclab1.erau.edu>
2021-03-23 14:35:29 -0700

    New arrow keys file

---
+++ register_arrow_input1.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+
+	int c ;
+  
+/* use system call to make terminal send all keystrokes directly to stdin */
+
+	system ("/bin/stty raw") ;
+
+	while ( (c=getchar() ) != '.' ) {
+
+    /* type a period to break out of the loop, since CTRL-D won't work raw */
+
+	switch (c) {
+	
+	case 'w' :
+		printf ("You pressed up") ;
+		break ;
+	
+	case 's' :
+		printf ("You pressed down") ;
+		break ;
+	
+	case 'd' :
+		printf ("You pressed right") ;
+		break ;
+	
+	case 'a' :
+		printf ("You pressed left") ;
+		break ;
+	
+	default :
+		printf ("") ;
+		break ;
+	}
+
+	}
+
+  /* use system call to set terminal behaviour to more normal behaviour */
+
+	system ("/bin/stty cooked") ;
+
+return 0 ;
+}
