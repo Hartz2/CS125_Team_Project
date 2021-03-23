86a9d11648ce5d3d1ad8fd1d221288acaebf8e45
Hartz2 <hartz2@my.erau.edu>
2021-03-23 14:10:26 -0700

    new functions

--- functionList.h
+++ functionList.h
@@ -1,1 +1,24 @@
+#define ROWS 16
+#define COLS 57
 void collide(int, int);
+void clearScreen();
+void printScreen();
+void printArray(char lines[ROWS][COLS]);
+int loop;
+char tower[ROWS][COLS]={
+     "          ____________________________________________ ",
+     "                                                     | ",
+     "                                                     | ",
+     "                                                     | ",
+     "                                                     | ",
+     "                                                     | ",
+     "                                                     | ",
+     "                                                     | ",
+     "                                                     | ",
+     "                                                     | ",
+     "                                                     | ",
+     "                                                     | ",
+     "                                                     | ",
+     "                                                     | ",
+     "          ___________________________________________| "};
+
