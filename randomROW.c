fa75781b26b5b2a1424cd34fdddcda7686fdbd5d
Isaiah Haliotis <80923384+Isaiah328@users.noreply.github.com>
2021-03-30 13:41:27 -0700

    Add files via upload

---
+++ randomROW.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+int main(){
+
+ int position;
+    srand(time(NULL));
+
+ while(enemy<COLS){
+    position= rand()%14+2;
+    enemy++;
+ }
+ return 0;
+}
+
+
