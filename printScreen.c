c75ffaded59a36b9e6877381364d7e85d8e6578d
Hartz2 <80923471+Hartz2@users.noreply.github.com>
2021-04-01 13:39:27 -0700

    Update printScreen.c

--- printScreen.c
+++ printScreen.c
@@ -1,25 +1,26 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 #include "functionList.h"
 
-void print_array(char lines[ROWS][COLS])
-{
-   
+int position=3;
+int playerPosition=8;
+
+void print_array(char lines[ROWS][COLS]){
+srand(time(NULL));
 int x;
- for (x=0; x< ROWS; x++)
- {   
+ for (x=0; x< ROWS; x++){
     printf("%s\n", lines[x]);
  }
 }
 
-void clearScreen()
-{
+void clearScreen(){
     usleep(1*200000);
     printf("\e[2J\e[H");
 }
 
-void printTower()
-{
+void printTower(){
     int loop;
     char tower[ROWS][COLS]={
      "          ____________________________________________ ",
@@ -39,26 +40,31 @@ void printTower()
      "          ___________________________________________| "};
     
     int enemyX;    
-    clearScreen();    
-    tower[8][COLS-5]='@';
-    print_array(tower);
+/*    clearScreen();        
+    tower[playerPosition][COLS-5]='@';
+    print_array(tower); */
 
-    for(enemyX=1; enemyX<(COLS-2); enemyX+=1)
-    {
-        clearScreen();
-        tower[position][enemyX]='*';
+    for(enemyX=1; enemyX<(COLS-2); enemyX+=1){	
+        clearScreen();	
+	playerPosition=rand()%15+1;
+	tower[playerPosition][COLS-5]='@';		
+	//usleep(1*20000);
+	//if (c!='w'||c!='s')
+	//{
+	//   c='k';
+	//}
+        tower[position][enemyX]='*';	
         print_array(tower);
+///////////////////[NEXT ITERATION]//////////////////////
+	tower[playerPosition][COLS-5]=' ';
         tower[position][enemyX]=' ';
     }
     
         
     
-    for(loop=0; loop<15; loop++)
-    {
+    for(loop=0; loop<15; loop++){
         printf("%s\n", tower[loop]);
     
     }
      printf("\n");     
 }
-
-
