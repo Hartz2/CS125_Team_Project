85015fbf659d3ab26ac699d011caf01c3978e80f
A-Morgan47 <80923478+A-Morgan47@users.noreply.github.com>
2021-04-06 14:40:49 -0700

    Update functionList.h

--- functionList.h
+++ functionList.h
@@ -1,10 +1,13 @@
 #define ROWS 16
 #define COLS 57
+
 void collide(int, int);
 void clearScreen();
 void printTower();
 void printArray(char lines[ROWS][COLS]);
+
 int loop;
+
 /*char tower[ROWS][COLS]={
      "          ____________________________________________ ",
      "                                                     | ",
