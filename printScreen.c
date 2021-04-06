ba025cf4e325653a45fa152294d358869635e899
A-Morgan47 <80923478+A-Morgan47@users.noreply.github.com>
2021-04-06 14:43:04 -0700

    Update printScreen.c

--- printScreen.c
+++ printScreen.c
@@ -9,10 +9,11 @@ int playerPosition=8;
 
 void print_array(char lines[ROWS][COLS]){
 srand(time(NULL));
+
 int x;
- for (x=0; x< ROWS; x++){
-    printf("%s\n", lines[x]);
- }
+	for (x=0; x< ROWS; x++) {
+    		printf("%s\n", lines[x]);
+ 	}
 }
 
 void clearScreen(){
@@ -44,7 +45,7 @@ void printTower(){
     tower[playerPosition][COLS-5]='@';
     print_array(tower); */
 
-    for(enemyX=1; enemyX<(COLS-2); enemyX+=1){	
+    for (enemyX=1; enemyX<(COLS-2); enemyX+=1) {	
         clearScreen();	
 	playerPosition=rand()%15+1;
 	tower[playerPosition][COLS-5]='@';		
@@ -62,9 +63,9 @@ void printTower(){
     
         
     
-    for(loop=0; loop<15; loop++){
-        printf("%s\n", tower[loop]);
+    for (loop=0; loop<15; loop++) {
+        printf ("%s\n", tower[loop]);
     
     }
-     printf("\n");     
+     printf ("\n");     
 }
